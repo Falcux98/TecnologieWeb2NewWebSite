13094e13bb12f726cab03e0953cfdaff5c68f21f
lorenzo Tribuiani <s1082416@studenti.univpm.it>
2020-05-23 16:25:52 +0200

    inizio controller

--- grp_14/www/laraProject/app/Http/Controllers/PublicController.php
+++ grp_14/www/laraProject/app/Http/Controllers/PublicController.php
@@ -6,25 +6,37 @@ implementeremo le classi del database */
 namespace App\Http\controllers;
 
 use App\Http\Controllers\Controller;
+use App\Catalog;
 
 class PublicController extends Controller{
 
-    protected $_catalogModel;
+    protected $_catalog;
 
     public function __construct()
     {
-
+        $this->_catalog = new Catalog;
     }
 
+    /*chiamata accesso al catalogo totale senza selezione di categoria, ritorna la vista con tutte le categorie */
     public function showMainCatalog(){
-
+        $products = $this->_catalog->getAllProds();
+        return view('catalog')
+        ->with('prods', $products);    
     }
 
-    public function showCatCatalog(){
+    public function showCatCatalog($categoryID){
+        $categories = $this->_catalog->getAllCategories();
 
+        $selectedCategory = $this->$categories->where('categoryID', $categoryID)->first();
+
+        $products = $this->_catalog->getProdsByCategoryID();
+
+        return view('catalog')
+            ->with('selectedCategory', $selectedCategory)
+            ->with('prods', $products);
     }
 
-    public function showSubCatCatalog(){
+    public function showSubCatCatalog($categoryID, $subCategoryID){
 
     }
 
