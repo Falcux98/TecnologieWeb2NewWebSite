ac5b0c9f9e7c99bc17865def9136fb51969365f2
lorenzo Tribuiani <s1082416@studenti.univpm.it>
2020-05-23 17:26:02 +0200

    public.blade.php e aggiunta css

---
+++ grp_14/www/laraProject/resources/views/public.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{ asset('css\animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('css\bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css\font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css\main.css') }}">
+    <link rel="stylesheet" href="{{ asset('css\prettyPhoto.css') }}">
+    <link rel="stylesheet" href="{{ asset('css\price-range.css') }}">
+    <link rel="stylesheet" href="{{ asset('css\price-range.css') }}">
+    <link rel="stylesheet" href="{{ asset('css\responsive.css') }}">
+    <title>TecWeb | @yeld('title', 'Home')</title>
+</head>
+<body>
+    
+</body>
+</html>
