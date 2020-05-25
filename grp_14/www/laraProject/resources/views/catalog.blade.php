171d0812a35184ccd7ffe34d8aab2e1becdf3423
lorenzo Tribuiani <s1082416@studenti.univpm.it>
2020-05-25 14:56:43 +0200

    lateral nav

--- grp_14/www/laraProject/resources/views/catalog.blade.php
+++ grp_14/www/laraProject/resources/views/catalog.blade.php
@@ -14,13 +14,13 @@
 			<div class="panel panel-default">
 				<div class="panel-heading">
 					<h4 class="panel-title">
-						<a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
+						<a data-toggle="collapse" data-parent="#accordian" href="#{{ str_replace(array(' ', '&') , '_',$category->nome) }}">
 							<span class="badge pull-right"><i class="fa fa-plus"></i></span>
 							{{ $category->nome}}
 						</a>
 					</h4>
 				</div>
-				<div id="sportswear" class="panel-collapse collapse">
+				<div id="{{ str_replace(array(' ', '&'), '_',$category->nome)  }}" class="panel-collapse collapse">
 					<div class="panel-body">
 						<ul>
 							@isset($subCategories)
@@ -37,11 +37,17 @@
 			@endforeach
 			@endisset
 
-			<!-- CATALOGO -->
+			<div class="price-range"><!--price-range-->
+				<h2>Prezzo</h2>
+				<div class="well">
+					 <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="600" data-slider-step="5" data-slider-value="[250,450]" id="sl2" ><br />
+					 <b>€ 0</b> <b class="pull-right">€ 1600</b>
+				</div>
+			</div><!--/price-range-->
 			
-
 		</div>	
 	</div>
+
     
 @endsection
 
