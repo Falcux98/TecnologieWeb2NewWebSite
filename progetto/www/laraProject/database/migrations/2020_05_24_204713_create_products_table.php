1ba3e2da2f4e4a246d02ce1c839af7b0b0dd31de
EnricoMartino <48827434+EnricoMartino@users.noreply.github.com>
2020-05-25 18:46:27 +0200

    aggiunta helpers

--- progetto/www/laraProject/database/migrations/2020_05_24_204713_create_products_table.php
+++ progetto/www/laraProject/database/migrations/2020_05_24_204713_create_products_table.php
@@ -14,7 +14,7 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id('codProdotto');
+            $table->id('codProdotto'); 
             $table->string('nome', 30);
             $table->string('descrizioneBreve', 30);
             $table->string('descrizioneEstesa', 500);
