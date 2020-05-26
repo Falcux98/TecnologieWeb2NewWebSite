92fad87f26c5cd2a9d890efa286fd28c17fa7b3f
Falcux98 <48416830+Falcux98@users.noreply.github.com>
2020-05-26 23:20:34 +0200

    aggiunta prima parte per l'amministrazione

---
+++ progetto/www/laraProject/app/Http/Controllers/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Admin;
+
+class AdminController extends Controller {
+
+    protected $_adminModel;
+
+    public function __construct() {
+        $this->_adminModel = new Admin;
+    }
+
+    public function index() {
+        return view('admin');
+    }
+
+}
--- progetto/www/laraProject/routes/web.php
+++ progetto/www/laraProject/routes/web.php
@@ -33,3 +33,4 @@ Route::view('/dovesiamo', 'dovesiamo') -> name('Dove Siamo');
 
 Route::view('/chisiamo', 'chisiamo') -> name('Chi Siamo');
 
+Route::get('/admin, AdminController@index') ->name('admin');
