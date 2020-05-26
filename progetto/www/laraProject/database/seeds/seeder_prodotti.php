e182fb8988232d13c53d8fbe42fa7a7a94790913
Luca Bastiani <luca.bastiani1997@gmail.com>
2020-05-26 18:00:26 +0200

    Aggiunti prodotti

--- progetto/www/laraProject/database/seeds/seeder_prodotti.php
+++ progetto/www/laraProject/database/seeds/seeder_prodotti.php
@@ -11,6 +11,32 @@ class seeder_prodotti extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('prodotto')->insert([
+            'codProdotto' => '01',
+            'nome' => 'Dell XPS 15',
+            'descrizioneBreve' => 'Costa na cifra',
+            'descrizioneEstesa' => 'Costa una cifra significa che costa tanto',
+            'prezzo' => '2000.50',
+            'foto' => 'public/images/products/diavola.jpg',
+            'percentualeSconto' => '15',
+            'inPromozione' => '1',
+            'categoria' => '01',
+            'sottocategoria' => '02'
+        ]);
+
+
+        DB::table('prodotto')->insert([
+            'codProdotto' => '02',
+            'nome' => 'Dell XPS 400',
+            'descrizioneBreve' => 'Costa na cifra',
+            'descrizioneEstesa' => 'Costa una cifra significa che costa tanto',
+            'prezzo' => '2000.50',
+            'foto' => 'public/images/products/diavola.jpg',
+            'percentualeSconto' => '10',
+            'inPromozione' => '0',
+            'categoria' => '01',
+            'sottocategoria' => '02'
+        ]);
+    
     }
 }
