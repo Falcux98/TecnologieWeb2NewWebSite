7585c5b9a62f1f7a7a50062138f5f9bedbbb58e0
EnricoMartino <48827434+EnricoMartino@users.noreply.github.com>
2020-05-28 18:41:24 +0200

    login

--- progetto/www/laraProject/app/Models/Utente.php
+++ progetto/www/laraProject/app/Models/Utente.php
@@ -1,10 +1,47 @@
 <?php
 
-namespace App;
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Utente extends Model
-{
-    protected $table = 'utente';
+class User extends Authenticatable {
+
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'username', 'nome', 'cognome', 'dataNascita', 'occupazione', 'residenza', 'password'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'username', 'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    
+    public function hasRole($role) {
+        $role = (array)$role;
+        return in_array($this->role, $role);
+    }
+
 }
