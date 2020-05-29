ab9e54e49286d1296dd1481e0c196f4b6bb4799e
Luca Bastiani <luca.bastiani1997@gmail.com>
2020-05-29 23:07:04 +0200

    Riorganizzazione

--- progetto/www/laraProject/resources/views/helpers/productPrice.blade.php
+++
@@ -1,6 +0,0 @@
-<p class="price"> {{ number_format($products->getPrice($products->inPromozione), 2, ',', '.') }} € </p>
-
-@if ($products->inPromozione == 1)
-<p class="discprice"> Valore <del>{{ number_format($products->getPrice(false), 2, ',', '.') }} €</del><br>
-    Sconto {{ $products->percentualeSconto }}%</p>
-@endif
