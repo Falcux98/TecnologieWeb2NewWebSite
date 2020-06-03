54ae9756cced8074de14820bd2400ab386acdbb8
EnricoMartino <48827434+EnricoMartino@users.noreply.github.com>
2020-06-03 21:23:31 +0200

    modifica css

--- progetto/www/laraProject/resources/views/helpers/productPrice.blade.php
+++ progetto/www/laraProject/resources/views/helpers/productPrice.blade.php
@@ -1,5 +1,5 @@
 @can('show-discount')
-    <p class="price"> {{ number_format($products->getPrice($products->inPromozione), 2, ',', '.') }} € </p>
+    <h4 style="color:black"> {{ number_format($products->getPrice($products->inPromozione), 2, ',', '.') }} € </h4>
     @if ($products->inPromozione == 1)
         <p class="discprice"> Valore <del>{{ number_format($products->getPrice(false), 2, ',', '.') }} €</del><br>
         Sconto {{ $products->percentualeSconto }}%</p>
