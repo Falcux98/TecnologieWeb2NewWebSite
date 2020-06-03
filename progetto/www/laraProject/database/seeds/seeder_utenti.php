4714481cef5786d459ab942a23c39c85c83163d8
Falcux98 <48416830+Falcux98@users.noreply.github.com>
2020-06-04 00:05:49 +0200

    fix seed

--- progetto/www/laraProject/database/seeds/seeder_utenti.php
+++ progetto/www/laraProject/database/seeds/seeder_utenti.php
@@ -53,7 +53,6 @@ class seeder_utenti extends Seeder
             'username' => 'useruser1',
             'role' => 'user',
             'password' => Hash::make('useruser'),
-            'foto' => '/laptop1.jpg',
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s")
         ]);
