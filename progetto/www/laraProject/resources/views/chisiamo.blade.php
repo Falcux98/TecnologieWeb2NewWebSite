cc2c95ff0775780a89e2f10a77edddad19b98c1a
EnricoMartino <48827434+EnricoMartino@users.noreply.github.com>
2020-06-07 20:17:33 +0200

    eliminazione immagini inutili e aggiornamento pagina chi siamo

--- progetto/www/laraProject/resources/views/chisiamo.blade.php
+++ progetto/www/laraProject/resources/views/chisiamo.blade.php
@@ -18,71 +18,50 @@
 
 @section('title', 'Chi Siamo')
 @section('content')
- <div id="contact-page" class="container">
+ <div id="contact-page" >
     	<div class="bg">
-	    	<div class="row">    		
+	    	  		
 	    		<div class="col-sm-12">    			   			
 					<h2 class="title text-center">Chi <strong>Siamo</strong></h2>    			    				    				
-					<div id="gmap" class="contact-map">
-                                            <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					<p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                        <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                        <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<div id="gmap" class="contact-map" style="font-size:18px">
+                                            <img src="images/home/light23113.jpg" style="height: 240px; width: 375px; float:left; margin: 18px">
+                                            <hr>
+                                            <p>TWeb è un sito di E-commerce che opera sul mercato dal 2008. Si propone come leader della rivendita di prodotti tecnologici e all'avanguardia.</p>
+                                            <p>Grazie alla sua notevole organizzazione è in grado di rispondere alle richieste degli utenti in tempi ristretti e di evolversi di pari passo con le esigenze del mercato maturando significative esperienze nell'ambito dell'evoluzione tecnologica.</p>
+                                            <p></p>
                                         </div>
                                         
+                                        
 				</div>			 		
-			</div>    
+			   
+          	
            
-    		<div class="row">  	
-	    		<div class="col-sm-8">
-	    			<div class="contact-form">
-	    				<h2 class="title text-center">Mettiti in contatto con noi</h2>
-	    				<div class="status alert alert-success" style="display: none"></div>
-				    	<form id="main-contact-form" class="contact-form row" name="contact-form" method="post">
-				            <div class="form-group col-md-6">
-				                <input type="text" name="name" class="form-control" required="required" placeholder="Nome">
-				            </div>
-				            <div class="form-group col-md-6">
-				                <input type="email" name="email" class="form-control" required="required" placeholder="Email">
-				            </div>
-				            <div class="form-group col-md-12">
-				                <input type="text" name="subject" class="form-control" required="required" placeholder="Oggetto">
-				            </div>
-				            <div class="form-group col-md-12">
-				                <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Scrivi qui il tuo messaggio..."></textarea>
-				            </div>                        
-				            <div class="form-group col-md-12">
-				                <input type="submit" name="submit" class="btn btn-primary pull-right" value="Invia">
-				            </div>
-				        </form>
-	    			</div>
-	    		</div>
-	    		<div class="col-sm-4">
+	    		<div style="text-align: center">
 	    			<div class="contact-info">
-	    				<h2 class="title text-center">Info Contatti</h2>
+	    				<h2 class="title text-center">Contatti</h2>
 	    				<address>
 	    					<p>TecnologieWeb shop Inc.</p>
 							<p>Via Brecce Bianche 12,  60131, AN</p>
 							<p>Ancona IT</p>
-							<p>Mobile: +39346 1234567</p>
+							<p>Mobile: +39 95 01 88 821</p>
 							<p>Fax: 0000000</p>
-							<p>Email: TecnologieWeb@univpm.it</p>
+							<p>Email: tecnologieweb@univpm.it</p>
 	    				</address>
 	    				<div class="social-networks">
 	    					<h2 class="title text-center">Social Networking</h2>
 							<ul>
 								<li>
-									<a href="#"><i class="fa fa-facebook"></i></a>
+									<a target="blank" href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a>
 								</li>
 								<li>
-									<a href="#"><i class="fa fa-twitter"></i></a>
+									<li><a target="blank" href="https://twitter.com/explore"><i class="fa fa-twitter"></i></a>
 								</li>
 								
 							</ul>
 	    				</div>
 	    			</div>
     			</div>    			
-	    	</div>  
+	   
     	</div>	
     </div>
 @endsection
