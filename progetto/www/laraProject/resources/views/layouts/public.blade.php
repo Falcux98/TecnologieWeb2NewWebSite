820b5834502336b853d6f231986d6b6ebbbc89bb
EnricoMartino <48827434+EnricoMartino@users.noreply.github.com>
2020-06-11 18:26:35 +0200

    rimozione termini e condizioni e aggiornato pdf relazione

--- progetto/www/laraProject/resources/views/layouts/public.blade.php
+++ progetto/www/laraProject/resources/views/layouts/public.blade.php
@@ -39,7 +39,7 @@
 						<div class="single-widget">
 							<h2>Policies</h2>
 							<ul class="nav nav-pills nav-stacked">
-								<li><a href="#">Termini di servizio</a></li>
+								
                                 <li><a href="{{ route('Privacy Policy') }}">Privacy Policy</a></li>
                                 <li><a href="{{ route('Relazione')}}">Relazione</a></li>
 							</ul>
